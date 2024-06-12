e2b34c512a07725cffcb7ae5281066e2a18b7166
Jose Fernando Chile Laime <jchilelaime38@gmail.com>
2024-06-11 22:14:11 -0400

    second commit

--- prediction-service/index.js
+++ prediction-service/index.js
@@ -1,8 +1,28 @@
 // prediction-service/index.js
 const { ApolloServer, gql } = require('apollo-server');
+const { RESTDataSource } = require('apollo-datasource-rest');
 const { buildFederatedSchema } = require('@apollo/federation');
 
+class ApiService extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'http://82.197.67.22:8080/api/';
+  }
+
+  async getAll() {
+    return this.get('getall');
+  }
+}
+
 const typeDefs = gql`
+  type ApiData {
+    id: Int
+    concepto: String
+    monto: Float
+    fecha: String
+    usuario: Int
+  }
+
   type Data {
     year: Int
     month: Int
@@ -22,17 +42,18 @@ const typeDefs = gql`
   extend type Query {
     predictBetweenDatesWithMonths(startDate: String!, numberOfMonths: Int!): PredictBetweenDatesWithMonths
     detectAnomalies: DetectAnomalies
+    getAll: [ApiData]
   }
 `;
 
 const resolvers = {
-    Query: {
-        predictBetweenDatesWithMonths: async (_, { startDate, numberOfMonths }) => {
-            const response = await fetch('http://82.197.67.22:8080/graphql', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    query: `
+  Query: {
+    predictBetweenDatesWithMonths: async (_, { startDate, numberOfMonths }) => {
+      const response = await fetch('http://82.197.67.22:8080/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          query: `
             query {
               predictBetweenDatesWithMonths(startDate: "${startDate}", numberOfMonths: ${numberOfMonths}) {
                 original_data {
@@ -48,17 +69,17 @@ const resolvers = {
               }
             }
           `,
-                }),
-            });
-            const result = await response.json();
-            return result.data.predictBetweenDatesWithMonths;
-        },
-        detectAnomalies: async () => {
-            const response = await fetch('http://82.197.67.22:8080/graphql', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    query: `
+        }),
+      });
+      const result = await response.json();
+      return result.data.predictBetweenDatesWithMonths;
+    },
+    detectAnomalies: async () => {
+      const response = await fetch('http://82.197.67.22:8080/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          query: `
             query {
                 detectAnomalies {
                     original_data {
@@ -74,18 +95,24 @@ const resolvers = {
                 }
             }
                 `,
-                }),
-            });
-            const result = await response.json();
-            return result.data.detectAnomalies;
-        },
+        }),
+      });
+      const result = await response.json();
+      return result.data.detectAnomalies;
     },
+    getAll: async (_, __, { dataSources }) => {
+      return dataSources.apiService.getAll();
+    },
+  },
 };
 
 const server = new ApolloServer({
-    schema: buildFederatedSchema([{ typeDefs, resolvers }])
+  schema: buildFederatedSchema([{ typeDefs, resolvers }]),
+  dataSources: () => ({
+    apiService: new ApiService(),
+  }),
 });
 
 server.listen({ port: 4001 }).then(({ url }) => {
-    console.log(`Prediction service ready at ${url}`);
+  console.log(`Prediction service ready at ${url}`);
 });
